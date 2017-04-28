f20dec05bdb738671d450b3c122cf9eaaa8fa897
Dmitry Patsura <talk@dmtry.me>
2017-04-28 15:47:02 +0900

    Welcome to App component

---
+++ App.js
@@ -0,0 +1,20 @@
+// @flow
+
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});

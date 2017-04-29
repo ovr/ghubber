50188dd724c9d288d06835053b02e1aa74801b11
Dmitry Patsura <talk@dmtry.me>
2017-04-29 20:30:12 +0900

    [Component] Avatar - initial :smiley_cat:

---
+++ components/Avatar/Avatar.js
@@ -0,0 +1,26 @@
+// @flow
+
+import React, { PureComponent } from 'react';
+import { Image } from 'react-native';
+
+// import flow types
+import type { UserEntity } from 'github-flow-js';
+
+type Props = {
+    user: UserEntity,
+    size: number,
+    style:? Object
+};
+
+export default class Avatar extends PureComponent<void, Props, void> {
+    render() {
+        const { user, size, style } = this.props;
+
+        return (
+            <Image
+                style={[{ width: size, height: size }, style]}
+                source={{ uri: `https://avatars0.githubusercontent.com/u/${user.id}?v=3` }}
+            />
+        )
+    }
+}

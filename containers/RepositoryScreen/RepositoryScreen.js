3a628483f40cf7a1044b36c07b981d9e0a93b5b7
Dmitry Patsura <talk@dmtry.me>
2017-04-30 07:54:27 +0900

    [RepositoryScreen] Initial

---
+++ containers/RepositoryScreen/RepositoryScreen.js
@@ -0,0 +1,50 @@
+// @author Dmitry Patsura <talk@dmtry.me> https://github.com/ovr
+// @flow
+
+import React, { PureComponent } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { IndicatorViewPager, PagerTitleIndicator } from 'rn-viewpager';
+import { connect } from 'react-redux';
+// import { Repository } from 'containers';
+
+type Props = {
+}
+
+class RepositoryScreen extends PureComponent<void, Props, void> {
+    render() {
+        return (
+            <IndicatorViewPager
+                style={ styles.viewPager }
+                indicator={
+                    <PagerTitleIndicator
+                        titles={
+                            ['Overview', 'Commits']
+                        }
+                    />
+                }
+            >
+                <View>
+                    <Text>Overview</Text>
+                </View>
+                <View>
+                    <Text>Commits</Text>
+                </View>
+            </IndicatorViewPager>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    viewPager: {
+        flex: 1,
+        flexDirection: 'column-reverse'
+    }
+});
+
+export default connect(
+    (state) => {
+        return {
+            navigation: state.navigation
+        }
+    }
+)(RepositoryScreen);
--- containers/index.js
+++ containers/index.js
@@ -5,3 +5,4 @@ export * from './Home';
 export * from './ProfileRepositories';
 export * from './ProfileScreen';
 export * from './Profile';
+export * from './RepositoryScreen';

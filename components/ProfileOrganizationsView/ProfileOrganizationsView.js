9a9ada865f3ede04dc3d074738631a671df01ca3
Dmitry Patsura <talk@dmtry.me>
2017-05-09 12:59:22 +0900

    [ProfileOrganizationsView] Optimize for Mobile/Tablet

--- components/ProfileOrganizationsView/ProfileOrganizationsView.js
+++ components/ProfileOrganizationsView/ProfileOrganizationsView.js
@@ -2,7 +2,8 @@
 // @flow
 
 import React, { PureComponent } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { isTablet } from 'react-native-device-info';
 import { OrganizationAvatar } from 'components';
 
 // import flow types
@@ -12,14 +13,27 @@ type Props = {
     organizations: Array<OrganizationEntity>
 };
 
+const { height, width } = Dimensions.get('window');
+
 export default class ProfileOrganizationsView extends PureComponent<void, Props, void> {
     render() {
         const { organizations } = this.props;
 
+        const organizationsCount = organizations.length;
+        const availableWidth = width - (10 * 2);
+        const availableElements = parseInt(availableWidth / (50 + ORG_AVATAR_MARGIN_RIGHT), 10);
+        const organizationsToRender = organizations.slice(0, availableElements);
+
+        let order = styles.rootFromStart;
+
+        if (!isTablet()) {
+            order = organizationsCount - availableElements > 0 ? styles.rootCenter : styles.rootCenter;
+        }
+
         return (
-            <View style={styles.root}>
+            <View style={[styles.root, order]}>
                 {
-                    organizations.map(
+                    organizationsToRender.map(
                         (organization) => (
                             <OrganizationAvatar
                                 key={organization.id}
@@ -34,13 +48,20 @@ export default class ProfileOrganizationsView extends PureComponent<void, Props,
     }
 }
 
+const ORG_AVATAR_MARGIN_RIGHT = 5;
+
 const styles = StyleSheet.create({
     root: {
         flex: 1,
         flexDirection: 'row',
+    },
+    rootFromStart: {
         justifyContent: 'flex-start'
     },
+    rootCenter: {
+        justifyContent: 'center'
+    },
     organizationAvatar: {
-        marginRight: 5
+        marginRight: ORG_AVATAR_MARGIN_RIGHT
     }
 })

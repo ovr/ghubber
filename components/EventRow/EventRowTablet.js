02414f08e402fbe3fbe8409118128be51243ef5f
Dmitry Patsura <talk@dmtry.me>
2017-05-28 02:27:05 +0900

    [Component] EventRow - show commits for PushEvent, refs #10

--- components/EventRow/EventRowTablet.js
+++ components/EventRow/EventRowTablet.js
@@ -21,13 +21,48 @@ function filterBranchName(refs: string): string {
 }
 
 export default class EventRowTablet extends PureComponent<void, Props, void> {
+    renderCommitsList(payload: Object): React.Element<any> | null {
+        if (!payload.commits) {
+            return null;
+        }
+
+        let commits = payload.commits;
+        let moreCommits = false;
+
+        if (payload.commits && payload.commits.length > 3) {
+            commits = payload.commits.slice(0, 3);
+            moreCommits = payload.commits.length - 3;
+        }
+
+        return (
+            <View style={styles.commitsList}>
+                {
+                    commits.map(
+                        (item) => {
+                            return (
+                                <View key={item.sha}>
+                                    <Text numberOfLines={1}>
+                                        <Text style={styles.commitSHA}>{item.sha.substring(0, 7)}</Text> {item.message}
+                                    </Text>
+                                </View>
+                            )
+                        }
+                    )
+                }
+                {
+                    moreCommits ? <Text style={styles.moreCommits}>({moreCommits}) commit(s) was hidden</Text> : null
+                }
+            </View>
+        )
+    }
+
     renderPushEvent(event: PushEvent | PullRequestEvent): React.Element<any> {
         return (
             <View style={styles.event}>
                 <View style={styles.left}>
                     <Icon name="git-commit" size={32} />
                 </View>
-                <View>
+                <View style={styles.right}>
                     <Text>
                         <Text style={styles.login}>{event.actor.login + " "}</Text>
                         pushed to <Text style={styles.branchName}>{filterBranchName(event.payload.ref)}</Text> at
@@ -35,6 +70,7 @@ export default class EventRowTablet extends PureComponent<void, Props, void> {
                     </Text>
                     <View style={styles.rightBottom}>
                         <Avatar user={event.actor} size={24} style={styles.avatar} />
+                        {this.renderCommitsList(event.payload)}
                     </View>
                 </View>
             </View>
@@ -101,7 +137,7 @@ export default class EventRowTablet extends PureComponent<void, Props, void> {
 
         return (
             <View style={styles.event}>
-                <Text>This type of event ({event.type}) is unsupported inside this version</Text>
+                <Text>This type of event ({event.type}) does not supported inside this version</Text>
             </View>
         )
     }
@@ -119,23 +155,36 @@ const styles = StyleSheet.create({
     left: {
         marginRight: 10
     },
+    right: {
+        flex: 1,
+    },
     rightBottom: {
         flex: 0,
         flexDirection: 'row',
         alignItems: 'center'
     },
     avatar: {
-        marginRight: 10
+        marginRight: 10,
     },
     login: {
-        fontSize: 18,
+        fontSize: 16,
         color: '#0366d6'
     },
     repoName: {
-        fontSize: 18,
+        fontSize: 17,
         color: '#0366d6'
     },
     branchName: {
         color: '#0366d6'
+    },
+    commitSHA: {
+        color: '#0366d6'
+    },
+    commitsList: {
+        flex: 1,
+    },
+    moreCommits: {
+        fontSize: 15,
+        fontWeight: 'bold'
     }
 });

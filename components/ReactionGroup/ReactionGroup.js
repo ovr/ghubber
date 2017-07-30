0846c96e2ff531dfa872730e28e918462006478f
Dmitry Patsura <talk@dmtry.me>
2017-07-31 01:15:45 +0900

    Welcome to component ReactionGroup

---
+++ components/ReactionGroup/ReactionGroup.js
@@ -0,0 +1,64 @@
+// @author Dmitry Patsura <talk@dmtry.me> https://github.com/ovr
+// @flow
+
+import React, { PureComponent } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { UIText } from 'components';
+
+type Props = {
+    reactions:? Array<Object>,
+};
+
+function mapReaction(name: string): string {
+    switch (name) {
+        case 'THUMBS_UP':
+            return '👍';
+        case 'THUMBS_DOWN':
+            return '👎';
+        case 'LAUGH':
+            return '😆 ';
+        case 'HOORAY':
+            return '🎉';
+        case 'CONFUSED':
+            return '😕';
+        case 'HEART':
+            return '💜';
+    }
+
+    return name;
+}
+
+export default class ReactionGroup extends PureComponent<void, Props, void> {
+    render() {
+        const { reactions } = this.props;
+
+        if (!reactions) {
+            return null;
+        }
+
+        return (
+            <View style={styles.reactions}>
+                {
+                    reactions.map(
+                        (reaction, index) => {
+                            return (
+                                <UIText key={'reaction' + index} style={styles.reaction}>{mapReaction(reaction.content)} {reaction.users.totalCount}</UIText>
+                            )
+                        }
+                    )
+                }
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    reactions: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    reaction: {
+        flex: 1,
+        flexDirection: 'row'
+    }
+});

8676c704c56e5a39a1ccea7c6b2de9512c1d9f44
Dmitry Patsura <talk@dmtry.me>
2017-09-14 23:49:27 +0900

    [NotificationRow] Render icon + time #13

--- components/NotificationRow/NotificationRow.js
+++ components/NotificationRow/NotificationRow.js
@@ -2,11 +2,14 @@
 // @flow
 
 import React, { PureComponent } from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { connect } from 'react-redux';
 import { UIText } from 'components';
 import { showRepositoryIssue, showRepositoryPullRequest } from 'actions';
 import { last } from 'lodash';
+
+import moment from 'utils/moment';
+import Icon from 'react-native-vector-icons/Octicons';
 
 // import flow types
 import type { NotificationEntity } from 'github-flow-js';
@@ -16,26 +19,6 @@ type Props = {
     showRepositoryIssue: typeof showRepositoryIssue,
     showRepositoryPullRequest: typeof showRepositoryPullRequest,
 };
-
-const styles = StyleSheet.create({
-    row: {
-        flex: 1,
-        paddingTop: 5,
-        paddingBottom: 5,
-    },
-    repositoryName: {
-        flex: 0,
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: '#586069',
-        marginRight: 5
-    },
-    title: {
-        flex: 0,
-        fontSize: 16,
-        fontWeight: 'bold',
-    }
-});
 
 export class NotificationRow extends PureComponent<Props> {
     onPress = () => {
@@ -62,17 +45,35 @@ export class NotificationRow extends PureComponent<Props> {
         }
     };
 
+    getIconName = () => {
+        const { notification } = this.props;
+
+        if (notification.subject.type === 'Issue') {
+            return 'issue-opened';
+        } else {
+            return 'git-pull-request';
+        }
+    };
+
     render() {
         const { notification } = this.props;
 
         return (
             <TouchableOpacity style={styles.row} onPress={this.onPress}>
-                <UIText style={styles.repositoryName} numberOfLines={1}>
-                    {notification.repository.name}
-                </UIText>
-                <UIText style={styles.title} numberOfLines={1}>
-                    {notification.subject.title}
-                </UIText>
+                <View style={styles.left}>
+                    <Icon
+                        name={this.getIconName()}
+                        size={24}
+                    />
+                </View>
+                <View style={styles.right}>
+                    <UIText style={styles.title} numberOfLines={1}>
+                        {notification.subject.title}
+                    </UIText>
+                    <UIText style={styles.updatedAt} numberOfLines={1}>
+                        {moment(notification.updated_at).fromNow()}
+                    </UIText>
+                </View>
             </TouchableOpacity>
         );
     }
@@ -82,3 +83,31 @@ export default connect(
     null,
     { showRepositoryIssue, showRepositoryPullRequest }
 )(NotificationRow);
+
+const styles = StyleSheet.create({
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    left: {
+        flex: 0,
+        flexDirection: 'row',
+        paddingRight: 4,
+        alignItems: 'center'
+    },
+    right: {
+        flex: 1,
+    },
+    title: {
+        flex: 0,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    updatedAt: {
+        flex: 0,
+        fontSize: 14,
+        color: '#586069',
+    },
+});

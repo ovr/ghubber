9fb1c0f2a63885f0a7508346e0ecc560a8cc4099
Dmitry Patsura <talk@dmtry.me>
2017-10-12 22:26:44 +0300

    [Refactoring] Export locales from one file

---
+++ locales/index.js
@@ -0,0 +1,15 @@
+// @flow
+
+import en from './en';
+import ru from './ru';
+import es from './es';
+import pt_br from './pt_br';
+import sk from './sk';
+
+export default {
+    en,
+    ru,
+    es,
+    pt_br,
+    sk
+};
--- utils/i18n.js
+++ utils/i18n.js
@@ -3,20 +3,14 @@
 import DeviceInfo from 'react-native-device-info';
 import I18n from 'i18n-js';
 
-import en from 'locales/en';
-import ru from 'locales/ru';
-import es from 'locales/es';
+import locales from 'locales';
 
 I18n.fallbacks = true;
 I18n.locale = DeviceInfo.getDeviceLocale().split('-')[0];
-I18n.translations = {
-    en,
-    ru,
-    es
-};
+I18n.translations = locales;
 
 I18n.pluralization['ru'] = function (count) {
-    var key = count % 10 == 1 && count % 100 != 11 ? 'one' : [2, 3, 4].indexOf(count % 10) >= 0 && [12, 13, 14].indexOf(count % 100) < 0 ? 'few' : count % 10 == 0 || [5, 6, 7, 8, 9].indexOf(count % 10) >= 0 || [11, 12, 13, 14].indexOf(count % 100) >= 0 ? 'many' : 'other';
+    const key = count % 10 == 1 && count % 100 != 11 ? 'one' : [2, 3, 4].indexOf(count % 10) >= 0 && [12, 13, 14].indexOf(count % 100) < 0 ? 'few' : count % 10 == 0 || [5, 6, 7, 8, 9].indexOf(count % 10) >= 0 || [11, 12, 13, 14].indexOf(count % 100) >= 0 ? 'many' : 'other';
     return [key];
 };
 

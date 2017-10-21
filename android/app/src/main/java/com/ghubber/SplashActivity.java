b0ba255fad0c07cbc4ed2a2d76ea3d91c07a83a6
Alexey Pyltsyn <lex61rus@gmail.com>
2017-10-21 22:18:41 +0300

    [Android] Add splash screen

---
+++ android/app/src/main/java/com/ghubber/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.ghubber;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    public static final int DELAY_MS = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        }, DELAY_MS);
+    }
+}

6f8c8c3f4f20a2e01d881c069134cd4d7241ae6e
Dmitry Patsura <talk@dmtry.me>
2017-10-22 13:41:15 +0300

    [Fix] Android: requestFeature() must be called before adding content

--- android/app/src/main/java/com/ghubber/SplashActivity.java
+++ android/app/src/main/java/com/ghubber/SplashActivity.java
@@ -13,7 +13,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 

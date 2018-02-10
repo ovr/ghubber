0721231daac4a8229a58d3e0564cec408d6aa108
Dmitry Patsura <talk@dmtry.me>
2018-02-10 18:37:17 +0900

    Feature: Introduce tryCatch helper fn

--- utils/action-helper.js
+++ utils/action-helper.js
@@ -4,7 +4,6 @@
 /**
  * ST is success type for Promise | Dispatch success
  */
-
 export function makeThunk<S>(
     cb: (state: State) => Promise<S>,
     type: ActionType,
@@ -35,4 +34,21 @@ export function makeThunk<S>(
             console.warn(e);
         }
     };
+}
+
+function tryCatchRejectDefault(e) {
+    console.warn(e);
+}
+
+export async function tryCatch<S>(
+    cb: () => Promise<S>,
+    resolve: (result: S) => any,
+    // You can override failure
+    reject?: (error: any) => any = tryCatchRejectDefault,
+): Promise<any> {
+    try {
+        return resolve(await cb());
+    } catch (e) {
+        reject(e);
+    }
 }
